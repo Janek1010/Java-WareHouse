c34ca5bee777115b95916f56f0f5ffa52bf97784
Janek1010 <janekdomozych99@gmail.com>
2023-10-02 21:51:03 +0200

    task 6 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,26 @@ import org.example.entities.Customer;
 import org.example.entities.Order;
 import org.example.entities.OrderDto;
 
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    private final static List<String> products = List.of("Piwo IPA","Piwo APA", "Pomidor", "Mango","Młotek");
-    private final static Map<String,String > map = new HashMap<>();
-    private final static List<String> names = List.of("Jan Kowalski","Zbigniew Noga", "Jacek Jackowski", "Marek Markowski","Jurek Jurkowski");
-    public static void main(String[] args) {
+    private final static List<String> products = List.of("Piwo IPA", "Piwo APA", "Pomidor", "Mango", "Młotek");
+    private final static Map<String, String> map = new HashMap<>();
+    private final static List<String> names = List.of("Jan Kowalski", "Zbigniew Noga", "Jacek Jackowski", "Marek Markowski", "Jurek Jurkowski");
+
+    public static void main(String[] args) throws FileNotFoundException {
 
         Random rand = new Random();
-        Set<Customer> customers  = new HashSet<>();
+        Set<Customer> customers = new HashSet<>();
 
-        for (String n: names) {
+        for (String n : names) {
             customers.add(Customer.builder().name(n).age(rand.nextInt(80) + 20).build());
         }
 
-        for (Customer c: customers) {
+        for (Customer c : customers) {
             for (int i = 0; i < rand.nextInt(5) + 3; i++) {
                 c.addOrder(Order.builder().customer(c).productName(products.get(rand.nextInt(5))).quantity(rand.nextInt(500) + 10).build());
             }
@@ -38,15 +40,44 @@ public class Main {
         //orders.forEach(System.out::println);
 
         // 4
-        orders = orders.stream().filter(el ->el.getQuantity() >= 80).filter(el ->el.getQuantity() < 250).sorted().collect(Collectors.toSet());
+        orders = orders.stream().filter(el -> el.getQuantity() >= 80).filter(el -> el.getQuantity() < 250).sorted().collect(Collectors.toSet());
         //orders.forEach(System.out::println);
 
         // 5
         List<OrderDto> dtos = orders.stream().map(el -> OrderDto.builder().customer(el.getCustomer().getName()).productName(el.getProductName()).quantity(el.getQuantity()).build()).toList();
-        dtos.forEach(System.out::println);
+        //dtos.forEach(System.out::println);
 
+        File file = new File("dataDTO.bin");
         // 6
+        //save
+        try (
+                FileOutputStream fos = new FileOutputStream(file);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ) {
+            oos.writeObject(dtos);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
+
+        List<OrderDto> readDTO = new ArrayList<>();
+
+        //read
+        FileInputStream fstream = new FileInputStream(file);
+        try {
+            ObjectInputStream ostream = new ObjectInputStream(fstream);
+
+            Object obj;
+            obj = ostream.readObject();
+            readDTO = (List<OrderDto>) obj;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        readDTO.forEach(System.out::println);
+
+        // 7
 
     }
 }
--- src/main/java/org/example/entities/OrderDto.java
+++ src/main/java/org/example/entities/OrderDto.java
@@ -3,12 +3,13 @@ package org.example.entities;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 
 @Builder
 @Data
-public class OrderDto {
+public class OrderDto implements Serializable {
     private String productName;
     private Integer quantity;
     private String customer;
